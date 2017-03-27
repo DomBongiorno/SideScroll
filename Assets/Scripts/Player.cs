d205964ae294f292b023460817395a76a8a998bf
Unknown <Dombonge24@yahoo.com>
2017-03-27 10:44:55 -0400

    Added Ability to move

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public int health = 100;
+
+	new Rigidbody2D rigidbody;
+
+
+	void Start ()
+	{
+		rigidbody = GetComponent<Rigidbody2D> ();
+	}
+	
+
+	void Update ()
+	{
+		float x = Input.GetAxisRaw ("Horizontal");
+		rigidbody.velocity = new Vector2 (x, 0);
+	}
+}
