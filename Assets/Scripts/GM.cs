ae55f1d7cb67842747bab2faa46c86fba0234f89
Unknown <Dombonge24@yahoo.com>
2017-04-05 16:18:05 -0400

    Added Lives/Score text

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GM : MonoBehaviour 
 {
@@ -8,15 +10,17 @@ public class GM : MonoBehaviour
 	public int lives;
 	public int points;
 	public GameObject Gameover;
+	public Text LivesValue;
+	public Text ScoreValue;  
 
 	public void SetLives(int newValue)
 	{
 		lives = newValue;
-		if (lives == 0) 
-		{
+		if (lives == 0) {
 			GameOver ();
 		}
 		Debug.Log ("Lives:" + lives);
+		LivesValue.text = lives.ToString ();
 	}
 
 	void GameOver()
