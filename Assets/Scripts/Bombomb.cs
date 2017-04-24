158c7010f5618d0c8cd2d3bd9949300b4b5e1619
Unknown <Dombonge24@yahoo.com>
2017-04-24 10:45:53 -0400

    Pick up and throw bomb

--- Assets/Scripts/Bombomb.cs
+++ Assets/Scripts/Bombomb.cs
@@ -6,22 +6,76 @@ public class Bombomb : MonoBehaviour
 {
 
 	public int radius = 10;
+	public bool isActive = false;
+
+	private new Rigidbody2D rigidBody2D;
+	private new Collider2D collider2D;
+
+
+	void Start()
+	{
+		rigidBody2D = GetComponent<Rigidbody2D> ();
+		collider2D = GetComponent<Collider2D> ();
+	}
+
+	void Update()
+	{
+		if (Input.GetButtonDown ("Fire1")&& isActive) 
+		{
+			Throw ();
+		}
+	}
 
 	void OnCollisionEnter2D(Collision2D coll)
 	{
 		var player = coll.gameObject.GetComponent<Player>();
-		gameObject.SetActive (false);
-		if (player != null) 
+
+		if (player != null && !isActive) 
 		{
-			var enemies = FindObjectsOfType<Enemy> ();
-			foreach (var e in enemies) 
+			PickUp (player);
+		}
+
+		if (player == null && isActive) 
+		{
+			explode ();
+		}
+	}
+
+	public void PickUp (Player player)
+	{
+		isActive = true;
+		transform.parent = player.transform;
+
+		transform.localPosition = new Vector3 (0.35f, 0.2f);
+
+		rigidBody2D.velocity = new Vector2 ();
+		rigidBody2D.isKinematic = true;
+		collider2D.enabled = false;
+			
+	}
+
+	public void Throw()
+	{
+		transform.parent = null;
+
+		rigidBody2D.velocity = new Vector2 (5,0);
+		rigidBody2D.isKinematic = false;
+		collider2D.enabled = true;	
+	}
+
+	public void explode()
+	{
+		var enemies = FindObjectsOfType<Enemy> ();
+		gameObject.SetActive (false);
+
+		foreach (var e in enemies) 
+		{
+			if (Vector3.Distance (this.transform.position, e.transform.position) < radius) 
 			{
-				if (Vector3.Distance (this.transform.position, e.transform.position) < radius) 
-				{
-					e.gameObject.SetActive (false);
-				}
+				e.gameObject.SetActive (false);
 			}
 		}
+
 	}
 	
 }
