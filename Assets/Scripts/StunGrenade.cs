a8e2ff4024fb65a56525305988425a4a8eb55162
Unknown <Dombonge24@yahoo.com>
2017-05-01 10:45:41 -0400

    Added Stun Grenade

--- Assets/Scripts/StunGrenade.cs
+++ Assets/Scripts/StunGrenade.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bombomb : Throwable 
-{
+public class StunGrenade : Throwable {
 
 	public int radius = 10;
 
 	void Update()
 	{
+
 	}
 
 	void OnCollisionEnter2D(Collision2D coll)
@@ -20,20 +20,31 @@ public class Bombomb : Throwable
 			explode ();
 		}
 	}
-		
+
 	public void explode()
 	{
 		var enemies = FindObjectsOfType<Enemy> ();
-		gameObject.SetActive (false);
+		collider2D.enabled = false;
+		GetComponent<SpriteRenderer> ().enabled = false;
 
 		foreach (var e in enemies) 
 		{
 			if (Vector3.Distance (this.transform.position, e.transform.position) < radius) 
 			{
-				e.gameObject.SetActive (false);
+				StartCoroutine( stun (e));
 			}
 		}
 
 	}
-	
+
+	IEnumerator stun(Enemy e)
+	{
+		var SR = e.GetComponent<SpriteRenderer> ();
+		SR.color = new Color (1, 1, 1, .5f);
+		e.enabled = false;
+		yield return new WaitForSeconds (3);
+
+		e.enabled = true;
+		SR.color = new Color (1, 1, 1, 1);
+	}
 }
---
+++ Assets/Scripts/Throwable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwable : Weapon {
+
+	public bool isActive = false;
+
+	public override void Attack()
+	{
+		transform.parent = null;
+
+		rigidBody2D.velocity = new Vector2 (5,0);
+		rigidBody2D.isKinematic = false;
+		collider2D.enabled = true;	
+	}
+
+	public override void PickUp (Player player)
+	{
+		if (isActive) 
+		{
+			return;
+		}
+		isActive = true;
+		base.PickUp (player);
+	}
+
+}
